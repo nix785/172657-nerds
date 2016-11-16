b6380b7dd56b9304ffd3891f421769825ba36dec
nix_785@rambler.ru <nix_785@rambler.ru>
2016-11-16 19:04:07 +0300

    Правки, почти финал

--- js/script.js
+++ js/script.js
@@ -17,18 +17,19 @@ var storName = localStorage.getItem("names");
 btn.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.add("modal_content_show");
-    
-  if (storName){
-    names.value = storName;
+
+    if (storName) {
+        names.value = storName;
         email.focus();
-    if (storEmail) {
-        email.value = storEmail;
-       message.focus();
+        if (storEmail) {
+            email.value = storEmail;
+            message.focus();
+        } else {
+            email.focus();
+        }
     } else {
-        email.focus();}
-    } 
-     else {names.focus();
-}
+        names.focus();
+    }
 });
 
 
@@ -36,25 +37,25 @@ btn.addEventListener("click", function (event) {
 close.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.remove("modal_content_show");
-    });
+});
 
-form.addEventListener("submit", function(event) {
-        if (!names.value) {
-          event.preventDefault();
-          names.style.borderColor="#e74246";
-            names.style.color="#ed7477";
-        } else {
-          localStorage.setItem("names", names.value);
-        };
-     if (!email.value) {
-          event.preventDefault();
-          email.style.borderColor="#e74246";
-            email.style.color="#ed7477";
-        } else {
-          localStorage.setItem("email", email.value);
-        }
+form.addEventListener("submit", function (event) {
+    if (!names.value) {
+        event.preventDefault();
+        names.style.borderColor = "#e74246";
+        names.style.color = "#ed7477";
+    } else {
+        localStorage.setItem("names", names.value);
+    };
+    if (!email.value) {
+        event.preventDefault();
+        email.style.borderColor = "#e74246";
+        email.style.color = "#ed7477";
+    } else {
+        localStorage.setItem("email", email.value);
+    }
     event.preventDefault();
-      });
+});
 
 window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
